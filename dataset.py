864fa4211a0856b01bfa5bf5cbaba6cb7785c85c
ivankunyankin <IKunyankin@gmail.com>
2021-07-19 13:44:31 +0300

    remove training from audio and normalisation

--- dataset.py
+++ dataset.py
@@ -1,10 +1,8 @@
 import re
-import os
 import torch
-import librosa
 import numpy as np
 from torch.utils.data import Dataset
-from utils import TextTransform, audio_to_mel, augment
+from utils import TextTransform, augment
 
 
 class LibriDataset(Dataset):
@@ -17,20 +15,6 @@ class LibriDataset(Dataset):
 
         self.label_encoder = TextTransform()
 
-        if not os.path.exists(self.parameters["data_list"]):
-            print(f"* List of {set} data not found. Generating...")
-            self.create_data_list()
-
-        if self.config["normalize"]:
-            if os.path.exists(self.config["stats"]):
-                stats = torch.from_numpy(np.load(self.config["stats"])).permute(1, 0)
-                if stats.shape[0] == 1:
-                    self.mean = stats[0, 0]
-                    self.std = stats[0, 1]
-                else:
-                    self.mean = stats[:, 0].unsqueeze(1)
-                    self.std = stats[:, 1].unsqueeze(1)
-
         with open(self.parameters["data_list"], "r") as f:
             data = f.readlines()
         data = [line.strip().split() for line in data]
@@ -41,27 +25,17 @@ class LibriDataset(Dataset):
         return len(self.collection)
 
     def __getitem__(self, item):
-        audio, transcript = self.collection[item]
+        spec_path, transcript_path = self.collection[item]
 
-        audio, _ = librosa.load(audio, sr=self.config["spec_params"]["sr"])
-        with open(transcript, "r") as text:
+        # read spectrograms
+        melspec = torch.from_numpy(np.load(spec_path))
+
+        # read and preprocess transcripts
+        with open(transcript_path, "r") as text:
             transcript = text.read()
             transcript = transcript.lower()
             transcript = re.sub("[^'A-Za-z0-9 ]+", '', transcript)
             transcript = torch.tensor(self.label_encoder.text_to_int(transcript), dtype=torch.long)
-
-        # apply time stretch
-        if self.parameters.get("apply_speed_perturbation", None):
-            limit = self.config.get("speed_perturbation", 0.1)
-            rate = np.random.uniform(low=1-limit, high=1+limit)
-            audio = librosa.effects.time_stretch(audio, rate)
-
-        # generate mel spectrogram
-        melspec = audio_to_mel(audio, self.config["spec_params"])
-
-        # apply normalization
-        if self.config["normalize"]:
-            melspec = (melspec - self.mean) / self.std
 
         # apply time and frequency masking
         if self.parameters.get("apply_masking", None):
@@ -71,30 +45,3 @@ class LibriDataset(Dataset):
         label_length = len(transcript)
 
         return melspec, transcript, input_length, label_length
-
-    def create_data_list(self):
-
-        data_list = open(self.parameters["data_list"], "w")
-
-        for folder in self.parameters["data_dirs"]:
-            for root, dirs, files in os.walk(folder):
-                for file in files:
-
-                    if file.endswith((".flac", ".wav")):
-
-                        audio, sr = librosa.load(os.path.join(root, file), sr=self.config["spec_params"]["sr"])
-
-                        # length check
-                        if self.config.get("max_length", None):
-                            length = audio.shape[0] / sr
-                            if length > int(self.config["max_length"]):
-                                continue
-
-                        # label check
-                        label = os.path.splitext(file)[0] + ".normalized.txt"
-                        if not os.path.exists(os.path.join(root, label)):
-                            continue
-
-                        data_list.write(f"{os.path.join(root, file)} {os.path.join(root, label)} {os.linesep}")
-
-        data_list.close()
---
+++ prepare_data.py
@@ -0,0 +1,64 @@
+import os
+import yaml
+import librosa
+import argparse
+import numpy as np
+
+from utils import audio_to_mel
+
+
+def main(config):
+
+    sets = ["train", "val", "test"]
+
+    for set_ in sets:
+
+        print(f"=> Preparing {set_} set...")
+
+        num_files = 0
+        num_valid = 0
+
+        data_list = open(config[set_]["data_list"], "w")
+
+        for folder in config[set_]["data_dirs"]:
+            for root, dirs, files in os.walk(folder):
+                for file in files:
+                    if file.endswith((".flac", ".wav")):
+                        num_files += 1
+
+                        audio, sr = librosa.load(os.path.join(root, file), sr=config["spec_params"]["sr"])
+
+                        # length check
+                        if config.get("max_length", None):
+                            length = audio.shape[0] / sr
+                            if length > int(config["max_length"]):
+                                continue
+
+                        # label check
+                        label = os.path.splitext(file)[0] + ".normalized.txt"
+                        if not os.path.exists(os.path.join(root, label)):
+                            continue
+
+                        melspec = audio_to_mel(audio, config["spec_params"])
+
+                        # save spectrogram
+                        spec_name = f"{'.'.join(file.split('.')[:-1])}.npy"
+                        np.save(os.path.join(root, spec_name), melspec)
+
+                        data_list.write(f"{os.path.join(root, spec_name)} {os.path.join(root, label)} {os.linesep}")
+                        num_valid += 1
+
+        data_list.close()
+        print(f"...overall files: {num_files}")
+        print(f"...valid files: {num_valid}")
+    print("=> Done!")
+
+
+if __name__ == "__main__":
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument('--conf', default="config.yml", help='Path to the configuration file')
+
+    args = parser.parse_args()
+    config = yaml.safe_load(open(args.conf))
